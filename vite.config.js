28622dbd4352e407d1ffdd201cb71997007d491d
factorprep-api <processone@pm.me>
2026-07-19 18:26:34 +0200

    Fix Konva in production build - add optimizeDeps and global definition

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['konva', 'react-konva']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        globals: {
+          konva: 'Konva'
+        }
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  define: {
+    global: 'window'
+  }
 })
