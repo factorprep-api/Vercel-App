0b70842e1f19fc25db1a58f9266d97f774e73472
factorprep-api <processone@pm.me>
2026-07-20 14:16:25 +0200

    Fix Konva crash - revert to simple vite config, use CSS bg only

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['konva', 'react-konva']
-  },
-  build: {
-    esbuild: {
-      keepNames: true
-    },
-    rollupOptions: {
-      output: {
-        globals: {
-          konva: 'Konva'
-        }
-      }
-    },
-    commonjsOptions: {
-      include: [/node_modules/]
-    }
-  },
-  define: {
-    global: 'window'
-  }
+  plugins: [react()]
 })
